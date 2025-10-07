71e30f46eefe070d5bfb5cc8f7fd35451a2596da
Jackson Costa <jacksonccosta@yahoo.com>
2025-10-07 19:59:03 -0300

    Estruturando a camada de domínio

---
+++ BookingService/Core/Domain/Entities/Room.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities;
+
+public class Room
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Level{ get; set; }
+    public bool InMaintenance { get; set; }
+    
+    public bool WasGuest 
+    {
+        //TODO: Implement logic to check if the room was ever booked by a guest
+        get { return true; } 
+    }
+
+    public bool IsAvailable {
+        get { 
+            if (InMaintenance || WasGuest) return false;
+            return true;
+        }
+    }
+}
