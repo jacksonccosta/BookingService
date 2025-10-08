3ea7ac6beaa0ec4cfc87e1e5950ca585450ceba1
Jackson Costa <jacksonccosta@yahoo.com>
2025-10-07 23:29:06 -0300

    Conectando com banco de dados e habilitando as migrations

---
+++ BookingService/Adapters/Data/HotelDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data;
+
+public class HotelDbContext(DbContextOptions<HotelDbContext> options) : DbContext(options)
+{
+    public virtual DbSet<Guest> Guests { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
