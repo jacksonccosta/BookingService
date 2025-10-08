2f0d6586a59cd7a5af70d7ce6d8f2c4aeb34a88b
Jackson Costa <jacksonccosta@yahoo.com>
2025-10-07 23:34:59 -0300

    Criando as Tabelas Faltantes

--- BookingService/Adapters/Data/HotelDbContext.cs
+++ BookingService/Adapters/Data/HotelDbContext.cs
@@ -6,6 +6,8 @@ namespace Data;
 public class HotelDbContext(DbContextOptions<HotelDbContext> options) : DbContext(options)
 {
     public virtual DbSet<Guest> Guests { get; set; }
+    public virtual DbSet<Room> Rooms { get; set; }
+    public virtual DbSet<Booking> Bookings { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
